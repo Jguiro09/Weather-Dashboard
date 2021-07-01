5ec053ad8a0005d246a452b3f9f3b68192a30388
Jacob Guiro <jguiro09@gmail.com>
2021-07-01 14:23:03 -0500

    Javascript pseudocoding done

---
+++ script.js
@@ -0,0 +1,29 @@
+// GIVEN a weather dashboard with form inputs
+
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// Weather one API? List Element, list.length remove child
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// Implemented in weather API? If not, array of the info, object grab and implement it into.
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// If (number) then (change background color)
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// Look inside API if they provide 5 day forcast, if not call for multiple days. Array object input for both. If statements for weather conditions. Array object input for temp wind and humidity.
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// Fetch when clicked. LI contains info
+// Capped at 5
+
+// API INFO ----------------------------------------------------------------------------
+
+// 5 Day / 3 Hour Forecast
+// Solar Radiation API
+// Current weather data
